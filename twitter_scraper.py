19bd9c9a74cb62246ae978d37f6ab8bb63b8195f
Tanay <tanayj2107@gmail.com>
2025-12-16 14:00:09 -0500

    created config file to store the creds

--- twitter_scraper.py
+++ twitter_scraper.py
@@ -4,21 +4,10 @@
 import praw
 import pandas as pd
 from datetime import datetime
-
-# API KEY: Su4IuUY49cwst266pC3bUnmBU
-# API SECRET KEY: CRLib1d2i9RwQmyBMOPmFivm0poND13jFB04YjkNex4OWez57E
-# BEARER TOKEN: AAAAAAAAAAAAAAAAAAAAAOG16AEAAAAAr50rVuBNCRzg3LS4mMXOpKlQ5%2Bw%3Dw27bbx6L1xQKmR88vbkbEpsWIH87lwkIjipBbhlD6USzljvkqo
+import tweepy
+import config
 
 def main():
-    # TODO: Go to https://developer.twitter.com/en/portal/dashboard
-    # TODO: Sign up for Twitter Developer account (free)
-    # TODO: Create a new app/project
-    # TODO: Get your API credentials:
-    #       - API Key
-    #       - API Secret Key
-    #       - Bearer Token
-    # TODO: Install Tweepy: pip install tweepy pandas
-    # TODO: Create config.py to store credentials (add to .gitignore!)
     # TODO: Test connection with simple script
     pass
 
