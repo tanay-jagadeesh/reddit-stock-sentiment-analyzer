82ec436217e082f0e1be2d176e3d4bafa87c7cdd
Tanay <tanayj2107@gmail.com>
2025-12-16 13:57:38 -0500

    got api key, secret key, and bearer token

--- twitter_scraper.py
+++ twitter_scraper.py
@@ -5,6 +5,10 @@ import praw
 import pandas as pd
 from datetime import datetime
 
+# API KEY: Su4IuUY49cwst266pC3bUnmBU
+# API SECRET KEY: CRLib1d2i9RwQmyBMOPmFivm0poND13jFB04YjkNex4OWez57E
+# BEARER TOKEN: AAAAAAAAAAAAAAAAAAAAAOG16AEAAAAAr50rVuBNCRzg3LS4mMXOpKlQ5%2Bw%3Dw27bbx6L1xQKmR88vbkbEpsWIH87lwkIjipBbhlD6USzljvkqo
+
 def main():
     # TODO: Go to https://developer.twitter.com/en/portal/dashboard
     # TODO: Sign up for Twitter Developer account (free)
