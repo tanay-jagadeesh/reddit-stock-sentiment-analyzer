d80afb58f1c070fb4eb1df18f68d9a99014ef7d2
Tanay <tanayj2107@gmail.com>
2025-12-16 15:33:39 -0500

    used requests.get to pull information from news api

---
+++ news_headline_scraper.py
@@ -0,0 +1,12 @@
+# News Sentiment Analyzer for Stock Mentions
+# Day 1: Set up News API
+import pandas as pd
+from datetime import datetime
+import requests
+import os
+from dotenv import load_dotenv
+import config 
+
+response = requests.get(f"https://newsapi.org/v2/everything?q=Apple&apiKey={config.API_KEY}")
+
+print(response)
--- twitter_scraper.py
+++
@@ -1,8 +0,0 @@
-# News Sentiment Analyzer for Stock Mentions
-# Day 1: Set up News API
-
-import pandas as pd
-from datetime import datetime
-import requests
-import os
-from dotenv import load_dotenv
