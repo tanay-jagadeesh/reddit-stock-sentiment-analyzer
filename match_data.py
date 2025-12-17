6d07bae3e6e82c7255fc9d6a1f7bd8c8403b3d8d
Tanay <tanayj2107@gmail.com>
2025-12-17 16:44:39 -0500

    extracting the date

--- match_data.py
+++ match_data.py
@@ -5,7 +5,16 @@ conn = sqlite3.connect('news.sentiment.db')
 c = conn.cursor()
 
 c.execute("SELECT * FROM articles ")
-print(c.fetchone())
+article = c.fetchone()
+
+# The published_at is at index 6
+published_at = article[6]
+print("Full timestamp:", published_at)
+
+# Extract just the date (first 10 characters)
+just_date = published_at[:10]
+print("Just the date:", just_date)
+
 
 
 
