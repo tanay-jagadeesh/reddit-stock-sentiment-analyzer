2734bcc73d9b6368799a6a9c8a8ac1c82b421603
Tanay <tanayj2107@gmail.com>
2025-12-21 11:43:10 -0500

    removed special characters

--- text_processor.py
+++ text_processor.py
@@ -4,3 +4,8 @@ import re
 def remove_url(txt, p):
     res = re.sub(p, "[URL REMOVED]", txt)
     return res
+
+def remove_special_characters(s):
+    res = re.sub(r'[^a-zA-Z0-9]', '', s)
+    return res
+
