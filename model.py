44a1b6ae2cf732f0505aee910122b7d72c0e5d43
Tanay <tanayj2107@gmail.com>
2025-12-25 00:57:55 -0500

    trained linear reg model

--- model.py
+++ model.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from sklearn.linear_model import LinearRegression
 
 # Load train/val/test sets
 X_train = pd.read_csv('X_train.csv')
@@ -9,3 +10,11 @@ y_train = pd.read_csv('y_train.csv')
 y_val = pd.read_csv('y_val.csv')
 y_test = pd.read_csv('y_test.csv')
 
+#created model
+reg_model = LinearRegression()
+
+#trained model
+reg_model.fit(X_train, y_train)
+
+#predictions
+reg_model_predictions = reg_model.predict(X_val)
